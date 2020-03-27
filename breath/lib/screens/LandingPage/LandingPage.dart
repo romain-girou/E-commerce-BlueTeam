03ca00019c28901e30ef314fff354de19f404b09
Calibanda <Calibanda@users.noreply.github.com>
2020-03-27 14:09:17 +0200

    Font modified

--- breath/lib/screens/LandingPage/LandingPage.dart
+++ breath/lib/screens/LandingPage/LandingPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
+
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
