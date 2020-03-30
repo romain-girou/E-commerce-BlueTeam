7fd7bf54b548b32d454bb055161058d74ea5c2bd
albus12cb11 <cbarbault.ing2022@esaip.org>
2020-03-30 18:30:10 +0300

    "Beta_v2_aboutUs"

--- breath/lib/screens/about_us/about_us_desktop.dart
+++ breath/lib/screens/about_us/about_us_desktop.dart
@@ -11,27 +11,68 @@ class AboutUsDesktop extends StatelessWidget {
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('Direction'),
+          SizedBox(height: 60),
+          Text('    Direction',
+              style: TextStyle(
+                fontFamily: 'Bell MT',
+                fontWeight: FontWeight.normal,
+                fontSize: 35,
+              )),
+          Divider(
+            endIndent: 10,
+            indent: 20,
+            color: Colors.grey[900],
+            height: 1,
+          ),
+          SizedBox(height: 60),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-            Image.asset(
-              'assets/images/ceo1.png',
-              width: 300,
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'assets/images/ceo2.png',
-              width: 300,
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'assets/images/ceo3.png',
-              width: 300,
-              fit: BoxFit.cover,
-            ),
-          ]),
-          Text('About Us'),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child:                   
+                Image.asset(
+                  'assets/images/ceo1.png',
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                ),
+                
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child:                   
+                Image.asset(
+                  'assets/images/ceo2.png',
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                ),
+                
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child:                   
+                Image.asset(
+                  'assets/images/ceo3.png',
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                ),
+
+              ]),
+          SizedBox(height: 30),
+          Text('    About Us',
+              style: TextStyle(
+                fontFamily: 'Bell MT',
+                fontWeight: FontWeight.normal,
+                fontSize: 35,
+              )),
+          Divider(
+            endIndent: 20,
+            indent: 20,
+            color: Colors.grey[900],
+            height: 1,
+          ),
+          SizedBox(height: 10),
           textSection,
         ],
       )
@@ -40,9 +81,11 @@ class AboutUsDesktop extends StatelessWidget {
 }
 
 Widget textSection = Container(
+  width: 700,
   padding: const EdgeInsets.all(32),
   child: Text(
-    'Text à propos de Nous',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta vestibulum nisl non commodo. Praesent vitae metus semper, ultrices est ut, placerat tortor. Proin in magna eu risus sagittis laoreet a vitae lacus. Praesent sed vehicula massa. Suspendisse suscipit dapibus dui quis pharetra. Donec bibendum lectus nec mi egestas porta. Aliquam malesuada et arcu id semper. Integer commodo nisi a justo luctus ultricies. Sed et eros quam. Donec quis placerat magna. Quisque eu nisl eget ex maximus malesuada. Praesent fringilla urna neque, sit amet tempus lorem faucibus nec. ',
+    textAlign: TextAlign.left,
     softWrap: true,
   ),
 );
