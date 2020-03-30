65df077f83eb6bb227410dadb494f44d06cc4fd1
albus12cb11 <cbarbault.ing2022@esaip.org>
2020-03-30 08:57:27 +0300

    nouveau fichier : lib/models/event/event.dart 	nouveau fichier : lib/models/user/user.dart 	nouveau fichier : lib/models/userData/userData.dart 	nouveau fichier : lib/services/search/search.dart 	nouveau fichier : lib/shares/constant.dart 	nouveau fichier : lib/shares/eventDetails/eventDetails.dart 	nouveau fichier : lib/shares/loading/loading.dart 	nouveau fichier : lib/shares/myAppBar/myAppBar.dart 	nouveau fichier : lib/shares/myBottomNaviagationBar/myBottomNavigationBar.dart 	nouveau fichier : lib/shares/route/route.dart

---
+++ breath/lib/models/userData/userData.dart
@@ -0,0 +1,23 @@
+class UserData {
+  final String username;
+  final String name;
+  final String nationnality;
+  final String appartment;
+  final String studies;
+  final String birthday;
+  final String description;
+  final String userImage;
+
+  UserData(
+      {this.username,
+      this.name,
+      this.nationnality,
+      this.appartment,
+      this.studies,
+      this.birthday,
+      this.description,
+      this.userImage,
+      });
+
+  
+}
