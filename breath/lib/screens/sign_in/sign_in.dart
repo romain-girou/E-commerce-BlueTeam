3721928f4b4f76359651fe5157d74f679152a5c9
albus12cb11 <cbarbault.ing2022@esaip.org>
2020-03-30 09:23:10 +0300

    modifié :         ../../../.dart_tool/package_config.json 	modifié :         ../../../android/local.properties 	modifié :         ../../../build/flutter_assets/LICENSE 	modifié :         sign_in.dart 	modifié :         ../../../pubspec.lock

--- breath/lib/screens/sign_in/sign_in.dart
+++ breath/lib/screens/sign_in/sign_in.dart
@@ -19,64 +19,64 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(height: 100.0),
-      endDrawer: MyDrawer(),
-      body: Center(
-        child: Container(
-          // color: Colors.grey[400],
-          decoration: BoxDecoration(
-            border: Border.all(),
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          ),
-          width: 500,
-          padding: EdgeInsets.fromLTRB(10, 50, 10, 0),
-          margin: EdgeInsets.fromLTRB(10, 20, 10, 50),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter an email',
-                    contentPadding: EdgeInsets.all(15.0),
-                    // border: InputBorder.none,
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
+        appBar: MyAppBar(height: 100.0),
+        endDrawer: MyDrawer(),
+        body: Center(
+            child: Container(
+                // color: Colors.grey[400],
+                decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 ),
-                SizedBox(height: 50,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter a password',
-                    contentPadding: EdgeInsets.all(15.0),
-                    // border: InputBorder.none,
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                  ),
-                  // The validator receives the text that the user has entered.
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 50,),
-                RaisedButton(
-                  onPressed: null
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                width: 500,
+                padding: EdgeInsets.fromLTRB(10, 50, 10, 0),
+                margin: EdgeInsets.fromLTRB(10, 20, 10, 50),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                              decoration: InputDecoration(
+                                  hintText: 'Enter an email',
+                                  contentPadding: EdgeInsets.all(15.0),
+                                  // border: InputBorder.none,
+                                  filled: true,
+                                  fillColor: Colors.grey[200],
+                              ),
+                              // The validator receives the text that the user has entered.
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                          ),
+                          SizedBox(height: 50,),
+                          TextFormField(
+                              decoration: InputDecoration(
+                                  hintText: 'Enter a password',
+                                  contentPadding: EdgeInsets.all(15.0),
+                                  // border: InputBorder.none,
+                                  filled: true,
+                                  fillColor: Colors.grey[200],
+                              ),
+                              // The validator receives the text that the user has entered.
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                          ),
+                          SizedBox(height: 50,),
+                          RaisedButton(
+                              onPressed: null
+                          )
+                              ],
+                              ),
+                              ),
+                              ),
+                              ),
+                              );
   }
 }
