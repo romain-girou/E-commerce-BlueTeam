197854d156119e75d15136abdb5fb0b41abda6a9
albus12cb11 <cbarbault.ing2022@esaip.org>
2020-04-06 11:14:36 +0300

    modifié :         ../.dart_tool/package_config.json 	modifié :         ../.packages 	modifié :         ../android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java 	modifié :         ../build/flutter_assets/LICENSE 	modifié :         ../ios/Runner/GeneratedPluginRegistrant.m 	modifié :         ../pubspec.lock 	modifié :         ../pubspec.yaml

--- breath/ios/Runner/GeneratedPluginRegistrant.m
+++ breath/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,9 +4,16 @@
 
 #import "GeneratedPluginRegistrant.h"
 
+#if __has_include(<square_in_app_payments/SquareInAppPaymentsFlutterPlugin.h>)
+#import <square_in_app_payments/SquareInAppPaymentsFlutterPlugin.h>
+#else
+@import square_in_app_payments;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SquareInAppPaymentsFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"SquareInAppPaymentsFlutterPlugin"]];
 }
 
 @end
